25e78f1e43f8c10e0cda3959383685d13bc9e23e
Piotr <pk.dydaktyka@gmail.com>
2018-08-20 13:40:24 +0200

    Laboratorium 3-4
    Mechaniz połączenia z bazą danych z wykorzystaniem biblioteki PDO.

--- class/Controllers/Main.php
+++ class/Controllers/Main.php
@@ -7,7 +7,22 @@
   final class Main extends Controller {
     public function __construct() {
         try {
-          d('Witaj Świecie!');
+          $pdo = \Handles\MySQL::getHandle();
+          try	{
+                $query = 'SELECT * FROM `produkty`';
+                $stmt = $pdo->prepare($query);
+      					if($stmt->execute())
+      					     $data = $stmt->fetchAll();
+      					$stmt->closeCursor();
+                d($data);
+            }
+            catch(\PDOException $e)	{
+                $data['error'] = \Config\Database\DBErrorName::$query;
+            }
+        } catch (\Exceptions\DatabaseConnection $e) {
+          $this->redirect('404.html');
+        } catch(\Exceptions\General $e) {
+          $this->redirect('404.html');
         } catch(\Exception $e) {
           $this->redirect('404.html');
         }
---
+++ messages/Error.php
@@ -0,0 +1,8 @@
+<?php
+	namespace Messages;
+
+
+	final class Error {
+    public static $unkonwn = 'Nieoczekiwany błąd aplikacji';
+    public static $databaseConnection = 'Błąd połączenia z bazą danych';
+	}
